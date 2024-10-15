5bd3b8a7b92689b09055aa39139e49685033f9ca
Ayush <iamayush02@gmail.com>
2024-10-15 19:05:17 +0530

    Result Page completed

--- screens/Result.js
+++ screens/Result.js
@@ -6,13 +6,37 @@ const Result = ({navigation}) => {
   const route = useRoute();
   const data = route.params;
   console.log('data', data);
+  // let temperature = data.data.main;
+  // temperature = Math.floor(temperature);
+  let weather;
   return (
-    <View>
-      <Text>Result</Text>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>Temperature in {} is </Text>
+        <Text style={styles.tempText}>{} degrees</Text>
+      </View>
+      <View>{/* <Text>{data.data.weather}</Text> */}</View>
     </View>
   );
 };
 
 export default Result;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00704a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tempText: {
+    fontSize: 38,
+    fontWeight: '800',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: '800',
+  },
+});
